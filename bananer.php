b4005ac430087041c68fe167e9ec59759bada56f
redoufu <nicidoggy@qq.com>
2016-01-20 22:44:37 +0800

    add a bannaner

---
+++ bananer.php
@@ -0,0 +1,19 @@
+<?php
+class apple{
+    public $color;
+    public $weight;
+    public $size;
+    
+    public function __construct($color, $weight, $size){
+        $this->color  = $color;
+        $this->weight = $weight;
+        $this->size   = $size;
+    } 
+    
+    public function getColor(){
+        return $this->color;
+    }
+
+}
+
+?>

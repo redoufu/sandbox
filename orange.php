54765eb5639a889a6131b27a13ea736a1b17536a
redoufu <nicidoggy@qq.com>
2016-01-20 23:23:30 +0800

    add an  orange

---
+++ orange.php
@@ -0,0 +1,22 @@
+<?php
+class Orange{
+    public $color;
+    public $weight;
+    public $size;
+    
+    public function __construct($color, $weight, $size){
+        $this->color  = $color;
+        $this->weight = $weight;
+        $this->size   = $size;
+    } 
+    
+    public function getColor(){
+        return $this->color;
+    }
+    
+    public function getWeight(){
+        return $this->weight;
+    }
+}
+
+?>
